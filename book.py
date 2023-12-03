f396671ebd677dc5b93c4a8fab0cfe8a1f61d0ac
mohylev <mohylev@gmail.com>
2023-12-03 12:50:35 +0100

    change title author year

--- book.py
+++ book.py
@@ -5,3 +5,10 @@ class Book:
         self.year = year
     def get_info(self):
         return f"Książka: {self.title} Autor: {self.author} Rok: {self.year}"
+    def change_title(self, new_title):
+        self.title = new_title
+    def change_author(self, new_author):
+        self.author = new_author
+    def change_year(self, new_year):
+        self.year = new_year
+
