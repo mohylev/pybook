36f2fba3ff3e24dd5dd25d5aaf7cd65f7a48cd10
mohylev <mohylev@gmail.com>
2023-12-03 13:26:00 +0100

    uzupelniene metod testowych

--- book_test.py
+++ book_test.py
@@ -8,6 +8,15 @@ class TestBook(unittest.TestCase):
         text_correct = "Książka: Wiedźmin Autor: Sapkowski Rok: 2000"
         text_result = self.book.get_info()
         self.assertEqual(text_result, text_correct)
+    def test_change_title(self):
+        self.book.change_title("Miecz przeznaczenia")
+        self.assertEqual("Miecz przeznaczenia", self.book.title)
+    def test_change_author(self):
+        self.book.change_author("Dick")
+        self.assertEqual("Dick", self.book.author)
+    def test_change_year(self):
+        self.book.change_year(3000)
+        self.assertEqual(3000, self.book.year)
 
 if __name__ == 'main':
     unittest.main()
